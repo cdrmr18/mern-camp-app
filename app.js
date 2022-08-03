777576b320bc2a800d351039da93e40676bdc475
Kathy Tavia <kathy.tavia@gmail.com>
2022-08-03 19:47:48 +0200

    added index route and page

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
 app.listen(3000, () => {
   console.log("Listening on 3000");
 });
