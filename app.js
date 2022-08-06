1720ae9581bed2e83472607add9f223d3a350805
Kathy Tavia <kathy.tavia@gmail.com>
2022-08-06 12:09:55 +0200

    added layout ejs boilerplate

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const morgan = require("morgan");
 
 mongoose
   .connect("mongodb://localhost:27017/campy")
@@ -18,6 +20,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 app.get("/", (req, res) => {
   res.render("home");
